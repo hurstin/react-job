d5a952ff29bc2cfc7b3d88bdbf47c9a72402a101
hurstin <132925282+hurstin@users.noreply.github.com>
2025-09-06 12:42:50 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import Hero from './components/Hero';
+import HomeCards from './components/HomeCards';
+import JobListings from './components/JobListings';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        tile="Become a React Dev"
+        subtitle="Find the React job that fits your skills and needs"
+      />
+      <HomeCards />
+      <JobListings />
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default App;
